aecf2488b00262292bd313deb6381eb843bb32b8
Dzmitry Lahunouski <killroy192@gmail.com>
2019-10-26 15:06:06 +0300

    initial

---
+++ README.md
@@ -0,0 +1,26 @@
+# Bootstrap for Jenkins
+
+Unofficial bootstrap for running your own [Jenkins](https://sentry.io/) with [Docker](https://www.docker.com/) and [docker-compose](https://docs.docker.com/compose/).
+
+## Requirements
+
+ * Docker 17.05.0+
+ * Compose 1.17.0+
+ * Traefik should be running with predefined network
+
+## Setup
+
+To get started with all the defaults, simply clone the repo and run `./install.sh` in your local check-out.
+
+The recommended way to customize your configuration is using the files below, in that order:
+
+ * `.env`
+ * `config/env/jenkins.env` w/ environment variables
+
+## Updating Sentry
+
+Just use the following steps to update:
+```sh
+docker-compose build --pull # Build the services again after updating, and make sure we're up to date on patch version
+docker-compose up -d # Recreate the services
+```
---
+++ install.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+set -e
+
+MIN_DOCKER_VERSION='17.05.0'
+MIN_COMPOSE_VERSION='1.17.0'
+JENKINS_ENV_FILE='.config/env/jenkins.env'
+ENV_FILE='.env.example'
+DOCKER_VERSION=$(docker version --format '{{.Server.Version}}')
+COMPOSE_VERSION=$(docker-compose --version | sed 's/docker-compose version \(.\{1,\}\),.*/\1/')
+
+DID_CLEAN_UP=0
+
+function ver () { echo "$@" | awk -F. '{ printf("%d%03d%03d", $1,$2,$3); }'; }
+
+function make_env() {
+    if [ -f "$@" ]; then
+        echo "$@ already exists, skipped creation."
+    else
+        echo "Creating $@..."
+        cp -n "$@.example" "$@"
+    fi   
+}
+
+# the cleanup function will be the exit point
+cleanup () {
+  if [ "$DID_CLEAN_UP" -eq 1 ]; then
+    return 0;
+  fi
+  echo "Cleaning up..."
+  docker-compose down &> /dev/null
+  DID_CLEAN_UP=1
+}
+trap cleanup ERR INT TERM
+
+echo "Checking minimum requirements..."
+
+if [ $(ver $DOCKER_VERSION) -lt $(ver $MIN_DOCKER_VERSION) ]; then
+    echo "FAIL: Expected minimum Docker version to be $MIN_DOCKER_VERSION but found $DOCKER_VERSION"
+    exit -1
+fi
+
+if [ $(ver $COMPOSE_VERSION) -lt $(ver $MIN_COMPOSE_VERSION) ]; then
+    echo "FAIL: Expected minimum docker-compose version to be $MIN_COMPOSE_VERSION but found $COMPOSE_VERSION"
+    exit -1
+fi
+
+echo ""
+echo "prepearing envs"
+make_env $JENKINS_ENV_FILE
+make_env $ENV_FILE
+echo ""
+
+echo ""
+echo "Creating volumes for persistent storage..."
+echo "Created $(docker volume create --name=jenkins-home)."
+echo ""
+
+echo ""
+echo "Building and tagging Docker images..."
+echo ""
+docker-compose build
+echo ""
+echo "Docker images built."
+
+cleanup
+
+echo ""
+echo "----------------"
+echo "You're all done! Run the following command get initial jenkins pass:"
+echo ""
+echo "  docker exec jenkins car var/jenkins_home"
+echo ""
